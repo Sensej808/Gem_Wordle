68b606ad746ec9b6d32d83ed9534dc6d5c3065d9
SskytT <regmail21@yandex.ru>
2023-05-31 16:54:16 +0300

    print colored text

--- lib/Wordle.rb
+++ lib/Wordle.rb
@@ -24,6 +24,18 @@ module Wordle
 
       # File.readlines( "#{__dir__ }/txt/#{@length}L.txt").sample
     end
+    private def grey(s)
+      "\e[37m#{s}\e[0m"
+    end
+    private def green(s)
+      "\e[32m#{s}\e[0m"
+    end
+    private def yellow(s)
+      "\e[33m#{s}\e[0m"
+    end
+    private def red(s)
+      "\e[31m#{s}\e[0m"
+    end
 
     private :pick_random_line
     def initialize(len = 5, attempts = 6)
@@ -47,12 +59,13 @@ module Wordle
       checker = db.execute("SELECT * FROM '#{@length}l' WHERE WORD='#{str}'")[0]
 
       raise NoMatchInDB, "there is no such word in database" if checker.nil?
-
+      puts red("YOU LOSE") if @attempts.zero?
       return [Array.new(str.length) { |i| [str[i], nil] }, :attempts_zero] if @attempts.zero?
-
       str.downcase!
       @attempts -= 1
       if str == @answer
+        puts (green(str))
+        puts (green("YOU WIN"))
         return [Array.new(str.length) { |i| [str[i], :green] }, :solved]
       end
 
@@ -69,8 +82,19 @@ module Wordle
       str.chars.each_with_index { |c, ind| res[ind][1] = :yellow  if !@answer.index(/[#{c}]/).nil? && res[ind][1].nil?  && marker[@answer.index(/[#{c}]/)] == false }
 
       str.chars.each_index { |ind| res[ind][1] = :grey if res[ind][1].nil? }
-
-
+      str_res = ""
+      for i in res
+        if i[1] == :grey
+          str_res += grey(i[0])
+        end
+        if i[1] == :yellow
+          str_res += yellow(i[0])
+        end
+        if i[1] == :green
+          str_res += green(i[0])
+        end
+      end
+      puts str_res
       return [res, :not_solved]
     end
   end
